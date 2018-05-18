009f3a8338d492738e28ecc3f505dff39ede18ca
git <se@studiomzk.com>
2018-05-18 10:30:48 +0200

    First commit.

---
+++ src/mathlib.py
@@ -0,0 +1,6 @@
+class Operations:
+    def sum(v1, v2):
+        if not isinstance(v1, int) and not isinstance(v2, int):
+            return False
+        else:
+            return v1 + v2
---
+++ test/mathlib_test.py
@@ -0,0 +1,9 @@
+import unittest
+from src.mathlib import Operations
+
+
+class OperationsTests(unittest.TestCase):
+    def test_sum(self):
+        # failed test
+        self.assertEqual(Operations.sum(3, 4), 8)
+
