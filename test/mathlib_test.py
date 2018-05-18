8d157d4e336bf1f31f96d26ac73a2527057fb7a2
git <se@studiomzk.com>
2018-05-18 13:24:37 +0200

    More test on Sum method.

--- test/mathlib_test.py
+++ test/mathlib_test.py
@@ -6,6 +6,6 @@ class OperationsTests(unittest.TestCase):
     def test_valid_sum(self):
         self.assertEqual(Operations.sum(3, 4), 7)
 
-    def test_invalid_sum(self);
-        self.assertFalse(Operations.sum(2.0, 3), 5.0)
+    def test_invalid_sum(self):
+        self.assertFalse(Operations.sum(2.0, 3.0), 5.0)
 
