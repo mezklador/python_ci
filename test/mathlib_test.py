688d52c4c0d0203e387600d3704e43264f9886f6
git <se@studiomzk.com>
2018-05-19 00:48:04 +0200

    New Assertion in Sum Test.

--- test/mathlib_test.py
+++ test/mathlib_test.py
@@ -5,6 +5,7 @@ from src.mathlib import Operations
 class OperationsTests(unittest.TestCase):
     def test_valid_sum(self):
         self.assertEqual(Operations.sum(3, 4), 7)
+        self.assertEqual(Operations.sum(3.0, 2), 5.0)
 
     def test_invalid_sum(self):
         self.assertFalse(Operations.sum(2.0, 3.0), 5.0)
